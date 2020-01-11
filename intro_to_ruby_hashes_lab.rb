5e34b1cc11e3973a5a8c757e97d67105b8e13b5e
Matt Milton <mattmilton57@gmail.com>
2020-01-11 23:52:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  if hash
+  if hash [key]
+    hash[key] +=1 
+  else
+    hash[key] = 1
 end
